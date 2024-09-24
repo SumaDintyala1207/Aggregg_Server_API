980e1db9dde397b8855f695b9f1e1bbb7df195cb
SumaDintyala1207 <a1950568@adelaide.edu.au>
2024-09-25 01:43:05 +0930

    GET request to aggregation server and weather data in JSON format are made.

--- ClientWeather.java
+++ ClientWeather.java
@@ -5,5 +5,29 @@ import java.util.Scanner;
 
 public class ClientWeather {
 
+    public static void main(String[] args) throws IOException {
+        String location = "NewYork";
+        String url = "http://localhost:8080/weather?location=" + location;
 
+        try {
+            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                Scanner scanner = new Scanner(connection.getInputStream());
+                while (scanner.hasNextLine()) {
+                    System.out.println(scanner.nextLine());
+                }
+                scanner.close();
+            } else {
+                System.out.println("Error: " + responseCode);
+            }
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
