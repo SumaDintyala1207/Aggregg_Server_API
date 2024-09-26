3a35ace249d5d53c5e9393f3af6c56e996f979db
SumaDintyala1207 <a1950568@adelaide.edu.au>
2024-09-26 16:06:01 +0930

    Basic structure of ContentServer. Errors not yet fixed.

--- ContentServer.java
+++ ContentServer.java
@@ -6,4 +6,23 @@ import java.nio.file.*;
 import java.util.concurrent.*;
 
 public class ContentServer {
+
+    private static final String FILE_PATH = "weather_data.json";
+    private static final String SERVER_URL = "http://localhost:8080/weather";
+    private static final int MAX_RETRIES = 3;
+
+    public static void main(String[] args) {
+        try{
+
+        } catch (IOException e) {
+
+        }
+    }
+
+    private static String readWeatherDataFromFile(String filepath) throws IOException {
+
+    }
+    public static boolean uploadWeatherData(String jsonData) {
+        int attempts = 0;
+    }
 }
